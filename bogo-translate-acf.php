8d042bacac9dcb8725ea521b8a0c0ddd62e5adea
faddi idris <foaddi@hotmail.com>
2014-06-16 12:39:23 +0200

    added support for non ajax loaded fields

--- bogo-translate-acf.php
+++ bogo-translate-acf.php
@@ -38,10 +38,10 @@ class Bogo_Acf {
      */
     function bogo_acf_select_default_template() {
         // only use select default template and load original post values if this is a page thats adds a translation
-        if( array_key_exists('locale', $_REQUEST) && array_key_exists('original_post', $_REQUEST) ) {
-            $original_post_id = $_REQUEST['original_post'];
-
-            $original_post_template = get_post_meta( $original_post_id, '_wp_page_template', true );
+        $locale = $this->get_get_or_referrer_key('locale');
+        $original_post = $this->get_get_or_referrer_key('original_post');
+        if( $locale && $original_post ) {
+            $original_post_template = get_post_meta( $original_post, '_wp_page_template', true );
 
             wp_enqueue_script( 'bogo_acf_js', plugin_dir_url( __FILE__ ) . '/bogo-translate-acf.js', 'jquery', '2013-09-11' );
 
@@ -55,9 +55,11 @@ class Bogo_Acf {
     function bogo_acf_select_parent_page( $dropdown_args, $post ) {
 
         // check if this is a page where we should be doing stuff
-        if( array_key_exists('locale', $_REQUEST) && array_key_exists('original_post', $_REQUEST) ) {
+        $locale = $this->get_get_or_referrer_key('locale');
+        $original_post = $this->get_get_or_referrer_key('original_post');
+        if( $locale && $original_post ) {
 
-            $original_post = get_post( $_REQUEST['original_post'] );
+            $original_post = get_post( $original_post );
 
             // check if the original post has a parent
             if( $original_post->post_parent != 0 ) {
@@ -67,7 +69,7 @@ class Bogo_Acf {
                     'meta_query'    => array(
                         array(
                             'key' => '_locale',
-                            'value' => $_REQUEST['locale']
+                            'value' => $locale
                         ),
                         array(
                             'key' => '_original_post',
@@ -89,22 +91,38 @@ class Bogo_Acf {
         return $dropdown_args;
     }
 
+    // Gets key_name from GET or HTTP_REFERER
+    // returns false if not found
+    function get_get_or_referrer_key($key_name){
+
+        $original_post = false;
+        if(isset($_GET[$key_name])){
+            $original_post = $_GET[$key_name];
+        } else {
+            $args = ( array_key_exists( 'HTTP_REFERER', $_SERVER ) ) ? wp_parse_args( $_SERVER['HTTP_REFERER'] ) : array();
+            if( array_key_exists($key_name, $args)){
+                $original_post = $args[$key_name];
+            }
+        }
+        return $original_post;
+    }
+
     function bogo_acf_load_original_post_values( $field ) {
 
-        $args = ( array_key_exists( 'HTTP_REFERER', $_SERVER ) ) ? wp_parse_args( $_SERVER['HTTP_REFERER'] ) : array();
+        $original_post = $this->get_get_or_referrer_key('original_post');
+        $locale = $this->get_get_or_referrer_key('locale');
         $original_post_field = array();
-
-        if( array_key_exists('locale', $args) && array_key_exists('original_post', $args) && !$this->bogo_acf_getting_field ) {
+        if( $locale && $original_post && !$this->bogo_acf_getting_field ) {
             $this->bogo_acf_getting_field = true;
 
-            $original_post_field = get_field_object( $field['key'], $args['original_post'], array( 'load_value' => true, 'format_value' => false ));
+            $original_post_field = get_field_object( $field['key'], $original_post, array( 'load_value' => true, 'format_value' => false ));
 
             switch( $original_post_field['type'] ) {
                 case 'repeater':
-                    $original_post_field['value'] = acf_field_repeater::format_value( $original_post_field['value'], $args['original_post'], $original_post_field );
+                    $original_post_field['value'] = acf_field_repeater::format_value( $original_post_field['value'], $original_post, $original_post_field );
                     break;
                 case 'flexible_content':
-                    $original_post_field['value'] = acf_field_flexible_content::format_value( $original_post_field['value'], $args['original_post'], $original_post_field );                                    
+                    $original_post_field['value'] = acf_field_flexible_content::format_value( $original_post_field['value'], $original_post, $original_post_field );
                     break;
                 default:
                     break;
